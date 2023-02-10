a3ea9d654fbcd872570e6cb978f4c64cf35e3fae
MaximeLCK <114729167+MaximeLCK@users.noreply.github.com>
2023-02-10 15:55:43 +0100

    catch err recup produits api

--- front/js/script.js
+++ front/js/script.js
@@ -8,4 +8,9 @@ fetch("http://localhost:3000/api/products")
   .then((objetProduits) => {
     // informations sur la console sous forme de tableau
     console.table(objetProduits); 
+  })
+  // si erreur on remplace le contenu de titles par un h1 "erreur 404" et renvoit l'erreur sur la console.
+  .catch((err) => {
+    document.querySelector(".titles").innerHTML = "<h1>erreur 404</h1>";
+    console.log("erreur 404, sur récupération produits api:" + err);
   });
