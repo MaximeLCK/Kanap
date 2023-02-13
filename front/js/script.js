01dc055dae39fc75f20d8ed617e29bac83620b09
MaximeLCK <114729167+MaximeLCK@users.noreply.github.com>
2023-02-13 09:42:12 +0100

    fonction affichage produit index

--- front/js/script.js
+++ front/js/script.js
@@ -8,9 +8,30 @@ fetch("http://localhost:3000/api/products")
   .then((objetProduits) => {
     // informations sur la console sous forme de tableau
     console.table(objetProduits); 
+    // appel de la fonction d'affichage des produits
+    affichageProduits(objetProduits);
   })
   // si erreur on remplace le contenu de titles par un h1 "erreur 404" et renvoit l'erreur sur la console.
   .catch((err) => {
     document.querySelector(".titles").innerHTML = "<h1>erreur 404</h1>";
     console.log("erreur 404, sur récupération produits api:" + err);
   });
+//----------------------------------------------------------------------
+// Fonction pour la structure HTML du produit sur index.html
+//----------------------------------------------------------------------
+function affichageProduits(index) {
+  // déclaration de variable pour la position des produits
+  const positionProduits = document.querySelector("#items");
+  // Boucle pour afficher les produits sur la page index.html
+  for (let structureProduits of index) {
+    // Afficher les produits sur la page index
+    positionProduits.innerHTML += `<a href="./product.html?_id=${structureProduits._id}">
+    <article>
+      <img src="${structureProduits.imageUrl}" alt="${structureProduits.altTxt}">
+      <h3 class="productName">${structureProduits.name}</h3>
+      <p class="productDescription">${structureProduits.description}</p>
+    </article>
+  </a>`;
+  }
+}
+//------------------------------------------------------------------------
